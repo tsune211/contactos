766d09215a765a3cea51affb25cf9c4ec74f5f2e
rodolfo mariscal <shinobukochou721@gmail.com>
2022-12-12 17:01:26 -0600

    validation

--- add.php
+++ add.php
@@ -2,14 +2,24 @@
 
   require "database.php";
 
+  $error = null;
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+   if (empty($_POST["name"]) || empty($_POST["phone_number"])){
+    $error = "please fill all the fields.";
+   }else{
     $name = $_POST["name"];
     $phoneNumber = $_POST["phone_number"];
 
-    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
+    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phoneNumber)");
+    $statement->bindParam(":name", $_POST["name"]);
+    $statement->bindParam(":phone_number", $_POST["phone_number"]);
     $statement->execute();
 
     header("Location: index.php");
+   }
+
   }
 ?>
 
@@ -78,6 +88,11 @@
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error): ?>
+                <p class="text-danger">
+                  <?php $error ?>
+                </p>
+              <? endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
